1fa500fd2a9ba0bd50590b2c849761202fa8941c
Kelvin Soares <kelvin.fe.soares@gmail.com>
2023-03-15 16:20:14 -0300

    Setup boilerplate
    Setup Vite + react + ts + tailwind + vitest + RTL

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import preview from '@/assets/preview/carbon.svg';
+import { Button } from 'antd';
+import themeConfig from './utils/utils';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center flex-col h-screen space-y-5 bg-dark-purple">
+      <h1 className="text-4xl font-bold text-linen text-center ">
+        Hello World
+      </h1>
+      <Button
+        type="primary"
+        className=" bg-teal-600 border-teal-600 text-white font-bold rounded-none "
+      >
+        Ant btn with a tailwind css styles
+      </Button>
+      <Button type="primary">Ant btn</Button>
+      <Button
+        type="primary"
+        style={{
+          background: themeConfig.theme.colors['slate-blue'],
+          borderColor: themeConfig.theme.colors['slate-blue'],
+        }}
+      >
+        Ant btn with a javascript variable colors
+      </Button>
+    </div>
+  );
+}
+
+export default App;
